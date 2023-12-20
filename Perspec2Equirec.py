6e4b45753151b04bdb8bb5c187a6adbfec52433f
Nhat Vu <53397242+nhatvu192@users.noreply.github.com>
2023-12-20 20:56:59 +0900

    Added docstring

--- Perspec2Equirec.py
+++ Perspec2Equirec.py
@@ -17,6 +17,42 @@ class Perspective:
         canvas_height: int,
         canvas_width: int,
     ) -> np.ndarray:
+        """
+        Given field of view, left/right and up/down viewing angle, the size of the
+        equirectangular canvas, and the size of the input perspective image,
+        return a 2D numpy array of shape (height, width, 2) where height and width
+        are the height and width of the perspective image such that each element (i,j)
+        contains the corresponding pixel index in the equirectangular canvas.
+
+        The steps performed in this function are as followed.
+        1. Normalize the distance from the center of the sphere to the perspective
+        image plane to 1 and center the perspective image onto the origin of x and y
+        coordinates
+
+        2. Rotate the plane by THETA and PHI.
+
+        3. Find the latitude and longitude coordinate of the projection of each
+        of the pixel on the perspective plane onto the sphere.
+
+        4. Convert the latitude and longitude to pixel coordinate in the equirectangular
+
+        Args:
+            FOV: angle of fiew of view
+
+            THETA: left/right viewing angle
+
+            PHI: up/down viewing angle
+
+            canvas_height: height of the equirectangular canvas to draw on
+
+            canvas_width: width of the equirectangular canvas to draw on
+
+        Returns:
+            a numpy array of shape (height, width, 2) where height and width
+            are the height and width of self.img and each pixel contains the
+            corresponding pixel in the equirectangular canvas.
+        """
+
         f = 0.5 * self.img_width * 1 / np.tan(0.5 * FOV / 180.0 * np.pi)
         cx = (self.img_width - 1) / 2.0
         cy = (self.img_height - 1) / 2.0
